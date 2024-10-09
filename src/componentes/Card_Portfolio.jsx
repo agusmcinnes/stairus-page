2acb3e7d8053dc48795e7293c6ea1381718f8dc0
Agus Mc Innes <agustinmcinnes@gmail.com>
2024-10-09 10:11:37 -0300

    animacion cards

--- src/componentes/Card_Portfolio.jsx
+++ src/componentes/Card_Portfolio.jsx
@@ -1,21 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
-const Card = ({mockUp, cardType, url, link}) => {
+const Card = ({ mockUp, cardType, url, link }) => {
     const type = cardType;
 
-    return (
-    <div key={Card}>
-        <a className='card' href={link} target='blank'>
-            <img src={mockUp} alt="" className='card__img'/>
-            <div className='card__info'>
-                <h3 className='card__type'>{type}</h3>
-                <h4 className='card__url'>{url}</h4>
-            </div>
+    useEffect(() => {
+        const skills = document.querySelectorAll('.aparecer__izquierda');
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('animacion__izquierda');
+                    observer.unobserve(entry.target); // Dejar de observar una vez que se ha animado
+                }
+            });
+        }, { threshold: 0.1 }); // Ajusta el threshold según sea necesario
 
-            
-        </a>
-    </div>
-)
-}
+        // Aplica el observer a cada card
+        skills.forEach(skill => observer.observe(skill));
+
+        // Cleanup para desconectar el observer cuando el componente se desmonte
+        return () => {
+            skills.forEach(skill => observer.unobserve(skill));
+        };
+    }, []);
+
+    return (
+        <div className='aparecer__izquierda'>
+            <a className='card' href={link} target='_blank' rel='noopener noreferrer'>
+                <img src={mockUp} alt="" className='card__img' />
+                <div className='card__info'>
+                    <h3 className='card__type'>{type}</h3>
+                    <h4 className='card__url'>{url}</h4>
+                </div>
+            </a>
+        </div>
+    );
+};
 
 export default Card;
